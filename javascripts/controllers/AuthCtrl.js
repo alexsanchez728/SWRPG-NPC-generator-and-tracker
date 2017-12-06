40052c43f83f9b072c39a094bc7a4607b999c055
Alex Sanchez <alesanchez728@gmail.com>
2017-12-05 19:28:16 -0600

    user can login

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -1,5 +1,15 @@
 'use strict';
 
 app.controller("AuthCtrl", function ($location, $rootScope, $scope, AuthService) {
-
+  $scope.controller = "AuthCtrl";
+  $scope.authenticate = () => {
+    AuthService.authenticateGoogle().then((result) => {
+      $rootScope.uid = result.user.uid;
+      $scope.$apply(() => {
+        $location.url("/battlePage");
+      });
+    }).catch((err) => {
+      console.log("error in authenticateGoogle", err);
+    });
+  };
 });
