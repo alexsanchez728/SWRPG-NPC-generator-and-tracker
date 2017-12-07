4e39e18dc2622d1aefc61c9effe256a16df8770d
Alex Sanchez <alesanchez728@gmail.com>
2017-12-07 14:43:41 -0600

    get units by uid works

--- javascripts/controllers/BattleCtrl.js
+++ javascripts/controllers/BattleCtrl.js
@@ -2,6 +2,16 @@
 
 app.controller("BattleCtrl", function ($location, $rootScope, $scope, BattleReadyUnitsService) {
 
+    const getMyBattleReadyUnits = () => {
+      BattleReadyUnitsService.getMyBattleReadyUnits($rootScope.uid).then((results) => {
+        $scope.units = results;
+      }).catch((error) => {
+        console.log("error in getMyBattleReadyUnits", error);
+      });
+    }; // end getMyBattleReadyUnits()
+
+    getMyBattleReadyUnits();
+
   const toCreate = () => {
     $location.path(`/newUnit1`);
   };
--- javascripts/services/BattleReadyUnitsService.js
+++ javascripts/services/BattleReadyUnitsService.js
@@ -1,5 +1,26 @@
 'use strict';
 
 app.service("BattleReadyUnitsService", function ($http, $q, FIREBASE_CONFIG) {
+  
+  const getMyBattleReadyUnits = (userUid) => {
+    let units = [];
+    return $q((resolve, reject) => {
+      console.log(userUid);
+      $http.get(`${FIREBASE_CONFIG.databaseURL}/battleReadyUnits.json?orderBy="uid"&equalTo="${userUid}"`).then((results) => {
+        let fbUnits = results.data;
+        if (fbUnits != null) {
+          Object.keys(fbUnits).forEach((key) => {
+            fbUnits[key].id = key;
+            units.push(fbUnits[key]);
+            resolve(units);
+          });
+        }
+      }).catch((error) => {
+        console.log("error in getMyBattleReadyUnits", error);
+      });
+    });
+  };
+
+  return { getMyBattleReadyUnits };
 
 });
