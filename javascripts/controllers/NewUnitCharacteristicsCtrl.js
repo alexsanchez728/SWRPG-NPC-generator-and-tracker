d8282e0a60a7de4cf38c55fd4b229e436a73f68a
Alex Sanchez <alesanchez728@gmail.com>
2017-12-06 21:26:13 -0600

    added dropdown for weapons

--- javascripts/controllers/NewUnitCharacteristicsCtrl.js
+++ javascripts/controllers/NewUnitCharacteristicsCtrl.js
@@ -1,16 +1,36 @@
 'use strict';
 
-app.controller("NewUnitCharacteristicsCtrl", function ($scope) {
-  $scope.create1 = "build-a-bad guy 1";
-  $scope.master = {};
+app.controller("NewUnitCharacteristicsCtrl", function ($rootScope, $scope, WeaponsService, GearAndEquipmentService) {
+
+  const getWeapons = () => {
+    WeaponsService.getAllWeapons().then((results) => {
+      console.log("results from weapons", results);
+      $scope.weapons = results;
+    }).catch((error) => {
+      console.log("error in getWeapons", error);
+    });
+  };
+  console.log("results from weapons", $scope.weapons);
+  getWeapons();
+
+  const getEquipment = () => {
+    GearAndEquipmentService.getAllGearAndEquipment().then((results) => {
+      $scope.equipments = results;
+    }).catch((error) => {
+      console.log("error in getEquipment", error);
+    });
+  };
+console.log("results from equipment", $scope.equipments);
+  getEquipment();
 
   $scope.addNew = (unitInfo) => {
     console.log("submitted", unitInfo);
   };
 
-
   $scope.reset = function () {
-    $scope.unitInfo = angular.copy($scope.master);
+    $scope.unitInfo = {};
   };
+
+
 
 });
