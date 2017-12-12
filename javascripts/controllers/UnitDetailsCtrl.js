04f270399bfbff2086ad669125604cd71bcc71c3
Alex Sanchez <alesanchez728@gmail.com>
2017-12-11 20:10:46 -0600

    unit's folder show up on page

--- javascripts/controllers/UnitDetailsCtrl.js
+++ javascripts/controllers/UnitDetailsCtrl.js
@@ -2,33 +2,6 @@
 
 app.controller("UnitDetailsCtrl", function ($location, $rootScope, $routeParams, $scope, FoldersService, GearAndEquipmentService, UnitsService, WeaponsService) {
 
-  const getSingleUnit = () => {
-    UnitsService.getUnit($routeParams.id).then((results) => {
-      $scope.unitInfo = results.data;
-    }).catch((error) => {
-      console.log("error in getSingleUnit", error);
-    });
-  }; // END GETUNIT()
-  getSingleUnit();
-
-  const getWeapons = () => {
-    WeaponsService.getAllWeapons().then((results) => {
-      $scope.weapons = results;
-    }).catch((error) => {
-      console.log("error in getWeapons", error);
-    });
-  };
-  getWeapons();
-
-  const getEquipment = () => {
-    GearAndEquipmentService.getAllGearAndEquipment().then((results) => {
-      $scope.equipments = results;
-    }).catch((error) => {
-      console.log("error in getEquipment", error);
-    });
-  };
-  getEquipment();
-
   const getFolders = () => {
     FoldersService.getAllMyFolders().then((results) => {
       $scope.folders = results;
@@ -37,6 +10,24 @@ app.controller("UnitDetailsCtrl", function ($location, $rootScope, $routeParams,
     });
   };
   getFolders();
+
+  const getSingleUnit = () => {
+    UnitsService.getUnit($routeParams.id).then((results) => {
+      $scope.unitInfo = results.data;
+      console.log("folder", $scope.unitInfo.folder);
+      console.log("folders in firebase", $scope.folders);
+      if ($scope.unitInfo.folder != undefined) {
+        $scope.folders.forEach((folder) => {
+          if ($scope.unitInfo.folder === folder.id) {
+            $scope.unitInfo.folderName = folder.name;
+          }
+        });
+      }
+    }).catch((error) => {
+      console.log("error in getSingleUnit", error);
+    });
+  }; // END GETUNIT()
+  getSingleUnit();
 
   $scope.toEditUnit = (id) => {
     $location.path(`/editUnit/${id}`);
